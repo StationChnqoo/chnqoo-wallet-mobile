2de257e3a098c9db45d8d450cfc8961243dc390c
孙宇鹏 <sunyp@bwoil.com>
2024-04-15 17:57:34 +0800

    🆕 新增首页两个菜单 ...

--- lib/pages/home/widgets/menus.dart
+++ lib/pages/home/widgets/menus.dart
@@ -3,47 +3,81 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeMenus extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
+  menuBuilder(context, String title, String message, String src, onPress) {
     return GestureDetector(
-      onTap: () {
-        Get.toNamed(RoutesClass.QUOTES);
-      },
+      onTap: onPress,
       child: Card.outlined(
         margin: EdgeInsets.zero,
         child: Container(
-          padding: EdgeInsets.all(12),
+          padding: EdgeInsets.all(8),
           child: Row(
             children: [
               Image.asset(
-                'assets/home_pencil.png',
-                height: 32,
-                width: 32,
+                src,
+                height: 28,
+                width: 28,
                 color: Theme.of(context).primaryColor,
               ),
               SizedBox(
-                width: 12,
+                width: 6,
               ),
-              Column(
+              Expanded(
+                  child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    '债券基金行情报表',
-                    style: TextStyle(fontSize: 16, color: Colors.black87),
+                    title,
+                    style: TextStyle(fontSize: 14, color: Colors.black87),
                   ),
                   SizedBox(
                     height: 2,
                   ),
                   Text(
-                    '一键生成当日债券基金行情',
-                    style: TextStyle(fontSize: 14, color: Colors.black54),
+                    message,
+                    maxLines: 1,
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black54,
+                        overflow: TextOverflow.ellipsis),
                   )
                 ],
-              )
+              ))
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Column(
+        children: [
+          menuBuilder(context, '行情', '债券基金实时指数、热门资讯', 'assets/home_pencil.png',
+              () {
+            Get.toNamed(RoutesClass.QUOTES);
+          }),
+          SizedBox(
+            height: 12,
+          ),
+          Row(
+            children: [
+              Flexible(
+                  flex: 3,
+                  child: menuBuilder(context, '收益', '债券基金收益、排行',
+                      'assets/home_pig.png', () {})),
+              SizedBox(
+                width: 12,
+              ),
+              Flexible(
+                  flex: 2,
+                  child: menuBuilder(
+                      context, '设置', '个性化服务', 'assets/home_skin.png', () {}))
+            ],
+          )
+        ],
       ),
     );
   }
